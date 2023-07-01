2dbe0a2963697a1b9a1a73c48a691e7c98b91075
Farhan Ahmed <89826402+faniAhmed@users.noreply.github.com>
2023-07-01 15:46:34 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""Install packages as defined in this file into the Python environment."""
+from setuptools import setup, find_packages
+
+setup(
+    name="Google Ads Transparency Scraper",
+    author="Farhan Ahmed",
+    author_email="jattfarhan10@gmail.com",
+    url="https://github.com/faniAhmed/GoogleAdsTransparencyScraper",
+    description="A scraper for getting Ads from Google Transparency",
+    version="1.5",
+    packages=find_packages(),
+    download_url= 'https://github.com/faniAhmed/GoogleAdsTransparencyScraper/archive/refs/tags/v1.2.tar.gz',
+    keywords= ['Google', 'Transparency', 'Scraper', 'API', 'Google Ads', 'Ads', 'Google Transparency', 'Google Transparency Scraper', 'Google Ads Scrapre'],
+    license='Securely Incorporation',
+    install_requires=[
+        "setuptools>=45.0",
+        "beautifulsoup4>=4.12.2",
+        "Requests>=2.31.0",
+        "lxml>=4.6.3",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: Free for non-commercial use",
+        "Natural Language :: Urdu",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    platforms=["any"],
+)
